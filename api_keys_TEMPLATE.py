8ad88f4723b297a08adebab14caedf058307e108
BasilBerg <vonpapenole@gmail.com>
2025-11-12 16:46:20 +0100

    added links for api key generation

--- api_keys_TEMPLATE.py
+++ api_keys_TEMPLATE.py
@@ -1,3 +1,4 @@
 #Insert your API keys and rename this file to api_keys.py
-TMDB_API_KEY = "" # Insert your TMDB API key here
-OMDB_API_KEY = "" # Insert your OMDB API key here
+#Both OMDb and TMDB offer a free tier for non-commercial use.
+TMDB_API_KEY = "" # Generate your API key here: https://www.themoviedb.org/settings/api
+OMDB_API_KEY = "" # Generate your API key here: https://www.omdbapi.com/apikey.aspx
